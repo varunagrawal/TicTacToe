b45aba82801801de43f73e9280d95909fafc4234
Varun Agrawal <varagrawal@gmail.com>
2013-07-29 01:46:38 +0530

    Better design

---
+++ TicTacToe_cpp/Tic_Tac_Toe_PlayerFirst.cpp
@@ -0,0 +1,101 @@
+/*
+Tic Tac Toe Program.
+Uses techniques discussed in a paper.
+Input is 'x y' where 1 <= input <= 3
+
+Author: Varun Agrawal
+*/
+
+
+#include "Tic_Tac_Toe.hpp"
+
+int won(int symbol){
+	int win = 0;
+	
+	for(int i=0;i<8;i++){
+		win = 0;
+		
+		for(int j=0;j<3;j++){
+			int x = trackCells[i][j];
+			
+			if(board[x/3][x%3] == symbol){
+				win += 1;
+			}
+			
+		}
+		
+		if(win == 3){
+			printf("%d\n", i);
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+
+int main()
+{
+	int player, opponent;
+	
+	player = 2;
+	opponent = 1;
+	
+	int cellsFilled = 0;
+	int o_move, p_move;
+	
+	display();
+	
+	while(cellsFilled < 9){
+		
+		if( won(player) ){
+			printf("Sorry! You lose...\n\n");
+			return 0;
+		}else if( won(opponent) ){
+			printf("You Win!!\n\n");
+			return 0;
+		}
+		
+		
+		vector<int> coord = prompt();
+		o_move = coord[0]*3 + coord[1];	
+				
+		board[o_move/3][o_move%3] = opponent;
+		
+		cellsFilled++;
+		
+		
+		if( (p_move = definitiveOffenseDefense(player)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+			
+		}else if( (p_move = definitiveOffenseDefense(opponent)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+		}else if( cellsFilled == 3 && (p_move = diagonalCorrection(player, opponent)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+		
+		}else if( (p_move = tentativeOffenseDefense(player)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+		
+		}else if( (p_move = tentativeOffenseDefense(opponent)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+		
+		}else if( (p_move = motion(player)) >= 0 ){
+			board[p_move/3][p_move%3] = player;
+		
+		}else{
+			p_move = prioritizedSelection();
+			board[p_move/3][p_move%3] = player;
+		}		
+		
+		cellsFilled++;
+
+		display();
+		
+		
+	}
+	
+	display();
+	
+	printf("It's a Draw!\n");		
+	return 0;
+}
